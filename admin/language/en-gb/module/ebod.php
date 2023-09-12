43cde8b6891658a08e9d4efbe6874cbf28ab1e59
ebodbot <142394627+ebodbot@users.noreply.github.com>
2023-09-12 14:04:03 +0200

    Create ebod.php

---
+++ admin/language/en-gb/module/ebod.php
@@ -0,0 +1,15 @@
+<?php
+$_['heading_title'] = 'EBOD';
+$_['entry_code'] = 'Website Token';
+$_['placeholder_name'] = '';
+$_['error_code'] = 'Website token is blank';
+/* Shown when at least 1 error occurred*/
+$_['error_warning'] = 'There are problems with data provided';
+$_['error_permission'] = 'You don\'t have permission to edit this.';
+$_['button_save'] = 'Save form';
+$_['button_cancel'] = 'Cancel';
+$_['text_extension'] = 'Extensions';
+$_['text_edit'] = 'Edit';
+$_['text_success'] = 'Saved';
+// Entry
+$_['entry_status']     = 'Status';
